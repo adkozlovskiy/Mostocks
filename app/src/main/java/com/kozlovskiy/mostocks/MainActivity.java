49e9f815a9d23dcae819954929824d01283138de
Alexey Kozlovsky <whiititi@gmail.com>
2021-02-16 19:30:40 +0300

    StockActivity layout beta.

--- app/src/main/java/com/kozlovskiy/mostocks/MainActivity.java
+++ app/src/main/java/com/kozlovskiy/mostocks/MainActivity.java
@@ -3,6 +3,7 @@ package com.kozlovskiy.mostocks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
